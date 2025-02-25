0b67fb465f7068a1178aa4afc0284c4ecfc56ba5
Your Name <youremail@niveussolutions.com>
2025-02-25 11:10:15 +0530

    Added file

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ const promiseMessage = () => {
 promiseMessage()
   .then((message) => console.log(message))
   .catch((error) => console.error('Error:', error));
+
+const fs = require('fs').promises;
+
+const createFile = async () => {
+  try {
+    await fs.writeFile('message.txt', 'Hello this is Node JS.');
+    console.log('File created successfully.');
+  } catch (error) {
+    console.error('Error creating file:', error);
+  }
+};
+
+createFile();
